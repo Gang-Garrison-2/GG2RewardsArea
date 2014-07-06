823c371b506d6c75fff7c16d213fa0fc1e022c86
Simeon Maxein <medo@ganggarrison.com>
2014-07-06 19:39:52 +0200

    Fixed redirect url from /index.php to ./index.php

--- frame.php
+++ frame.php
@@ -7,4 +7,4 @@ $query_string = $_SERVER['QUERY_STRING'];
 ?><!doctype html>
 <meta charset=utf-8>
 <title>GG2 Rewards Area Redirection Frame</title>
-<iframe src="/index.php?<?=htmlspecialchars($query_string)?>" width="100%" height="100%" frameborder="0" style="position: fixed; left: 0; right: 0; top: 0; bottom: 0;"></iframe>
+<iframe src="./index.php?<?=htmlspecialchars($query_string)?>" width="100%" height="100%" frameborder="0" style="position: fixed; left: 0; right: 0; top: 0; bottom: 0;"></iframe>
